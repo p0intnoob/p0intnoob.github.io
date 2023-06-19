8860f2974ef6f9b7535bc204e1874c0671a4d78b
Edison Rojas Bravo <cristoferrojasbravo@gmail.com>
2023-06-19 16:25:10 -0400

    Entrega uno
    plataforma en uso

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,8 @@
 (function(){
     const openButton = document.querySelector('.nav__menu');
-    const menu = document.querySelector('nav__link');
-    const closeMenu =document.querySelector('.nav__close');
-
-    openButton.addEventListener('click',()=>{
-        menu.classList.add('nav__link--show');
+    openButton.addEventListener('click', ()=> {
+        alert('Remember to subscribe!');
     });
-
-    closeMenu.addEventListener('click',()=>{
-        menu.classList.remove('nav__link--show');
-    });
-
+    
 
 })();
